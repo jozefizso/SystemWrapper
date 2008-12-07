f8bbb0d25bd82c0d3cc13ff9b49899a069d92369
Vadim Kreynin <vkreynin@gmail.com>
2008-12-07 17:49:29 +0100

    Started working on AppDomain class.

---
+++ SystemWrapper/AppDomainWrap.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Runtime.InteropServices;
+using SystemWrapper.Reflection;
+
+namespace SystemWrapper
+{
+    /// <summary>
+    /// Wrapper for <see cref="System.AppDomain"/> class.
+    /// </summary>
+    [ComVisible(true)] 
+    [Serializable]
+    public class AppDomainWrap : IAppDomainWrap
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:SystemWrapper.AppDomainWrap"/> class. 
+        /// </summary>
+        /// <param name="appDomain">AppDomain object.</param>
+        public AppDomainWrap(AppDomain appDomain)
+        {
+            AppDomainInstance = appDomain;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:SystemWrapper.AppDomainWrap"/> class and creates a new application domain with the specified name.
+        /// </summary>
+        /// <param name="friendlyName">The friendly name of the domain.</param>
+        public AppDomainWrap(string friendlyName)
+            : this(AppDomain.CreateDomain(friendlyName))
+        {}
+
+        public AppDomain AppDomainInstance { get; private set; }
+
+        public IAppDomainWrap CurrentDomain
+        {
+            get { return new AppDomainWrap(AppDomain.CurrentDomain); }
+        }
+
+        public object GetData(string name)
+        {
+            return AppDomainInstance.GetData(name);
+        }
+
+        public IAssemblyWrap Load(IAssemblyNameWrap assemblyRef)
+        {
+            return new AssemblyWrap(AppDomainInstance.Load(assemblyRef.AssemblyNameInstance));
+        }
+
+        public void SetData(string name, object data)
+        {
+            AppDomainInstance.SetData(name, data);
+        }
+
+        public void Unload(IAppDomainWrap domain)
+        {
+            AppDomain.Unload(domain.AppDomainInstance);
+        }
+
+        event ResolveEventHandler IAppDomainWrap.AssemblyResolve
+        {
+            add { AppDomainInstance.AssemblyResolve += value; }
+            remove { AppDomainInstance.AssemblyResolve -= value; }
+        }
+    }
+}
