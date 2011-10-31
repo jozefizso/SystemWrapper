f6b9b4a44ba6312fc2e82b10c9b664115bcb65d2
Jared Barneck <rhyous@rhyous.com>
2011-10-31 22:21:14 +0100

    Made IAccessTheRegistry public and updated comments

--- SystemWrapper4/SystemInterface/Microsoft.Win32/IAccessTheRegistry.cs
+++ SystemWrapper4/SystemInterface/Microsoft.Win32/IAccessTheRegistry.cs
@@ -4,19 +4,41 @@
     /// This is an interface that demonstrates a good practice
     /// for implementing a class that accesses the registry.
     /// </summary>
-    interface IAccessTheRegistry
+    public interface IAccessTheRegistry
     {
         /// <summary>
         /// This property allows for passing in a Mock registry key
         /// object that implements IRegistryKey to Mock the registry.
-        /// By default though, it will use the same
+        /// The implementor should use RegistryKeyWrap in production.
+        /// 
+        /// Example Usage:
+        ///        
+        /// IRegistryKey BaseKey
+        /// {
+        ///    get
+        ///    { 
+        ///        if (null == _BaseKey)
+        ///            _BaseKey = new new RegistryWrap().LocalMachine;
+        ///        return _BaseKey;
+        ///    }
+        /// } private IRegistryKey _BaseKey;
         /// </summary>
         IRegistryKey BaseKey { get; }
 
+
+
         /// <summary>
         /// This allows for switching the BaseKey to a different IRegistry object.
-        /// It is expected this will only be used to mock teh registry for unit testing.
-        /// We use this function so that the property can remain read only.
+        /// It is expected this will only be used to mock the registry for unit testing.
+        /// We use a function because it is intended that the property should remain 
+        /// read only.
+        /// 
+        /// Example Usage:
+        /// 
+        /// public void ChangeBaseKey(IRegistryKey inBaseKey)
+        /// {
+        ///     _BaseKey = inBaseKey;
+        /// }
         /// </summary>
         /// <param name="inBaseKey"></param>
         void ChangeBaseKey(IRegistryKey inBaseKey);
