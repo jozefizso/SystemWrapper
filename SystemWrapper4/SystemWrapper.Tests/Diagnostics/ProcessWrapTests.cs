6f2a96ba2749b27403ac266a296d3a5e3a1a86f1
Jared Barneck <rhyous@rhyous.com>
2014-02-06 18:40:00 +0100

    Added Basic Test for TraceSource

--- SystemWrapper4/SystemWrapper.Tests/Diagnostics/ProcessWrapTests.cs
+++ SystemWrapper4/SystemWrapper.Tests/Diagnostics/ProcessWrapTests.cs
@@ -4,32 +4,31 @@ using Rhino.Mocks;
 
 namespace SystemWrapper.Tests.IO
 {
-	[TestFixture]
-	[Author("Brad Irby", "Brad@BradIrby.com")]
-	public class ProcessWrapTests
-	{
+    [TestFixture]
+    [Author("Brad Irby", "Brad@BradIrby.com")]
+    public class ProcessWrapTests
+    {
 
-			[SetUp]
-			public void Setup()
-			{
-			}
+        [SetUp]
+        public void Setup()
+        {
+        }
 
-			[Test]
-			public void Constructor_Sets_ProcessInstance()
-			{
-				var instance = new ProcessWrap();
-				Assert.IsNotNull(instance.ProcessInstance);
-			}
+        [Test]
+        public void Constructor_Sets_ProcessInstance()
+        {
+            var instance = new ProcessWrap();
+            Assert.IsNotNull(instance.ProcessInstance);
+        }
 
-			[Test]
-			public void Initializer_Sets_ProcessInstance()
-			{
-				var instance = new ProcessWrap();
-				var origInfo = instance.ProcessInstance;
-				instance.Initialize();
-				Assert.AreNotSame(origInfo, instance.ProcessInstance);
-				Assert.IsNotNull(instance.ProcessInstance);
-			}
-
-	}
+        [Test]
+        public void Initializer_Sets_ProcessInstance()
+        {
+            var instance = new ProcessWrap();
+            var origInfo = instance.ProcessInstance;
+            instance.Initialize();
+            Assert.AreNotSame(origInfo, instance.ProcessInstance);
+            Assert.IsNotNull(instance.ProcessInstance);
+        }
+    }
 }
