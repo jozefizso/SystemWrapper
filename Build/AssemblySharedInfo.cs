b8207e13022fd31b1c56ea47dd0bdd0728804a7a
Jozef Izso <jozef.izso@gmail.com>
2014-10-01 19:13:44 +0200

    Assembly information cleanup.

---
+++ Build/AssemblySharedInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// Shared information about assemblies in the SystemWrapper solution.
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SystemWrapper")]
+[assembly: AssemblyCopyright("Copyright (c) 2008 Jared Barneck, Portions Copyright (c) 2014 Jozef Izso")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Disable COM visibility
+[assembly: ComVisible(false)]
+
+// NOTE: Assembly name is set in respective project files.
+[assembly: AssemblyVersion("0.6.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
