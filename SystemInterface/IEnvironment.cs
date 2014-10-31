ecda648a0cc4f3293ca982de042c6b8c4758612b
Jozef Izso <jozef.izso@gmail.com>
2014-10-31 17:42:40 +0100

    Simple interface for Environment class.

---
+++ SystemInterface/IEnvironment.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SystemInterface
+{
+    /// <summary>
+    /// Provides information about, and means to manipulate, the current environment and platform.
+    /// </summary>
+    /// <remarks>
+    /// This interface represents the <see cref="Environment"/> class.
+    /// </remarks>
+    public interface IEnvironment
+    {
+        /// <summary>
+        /// Determines whether the current operating system is a 64-bit operating system.
+        /// </summary>
+        /// <returns>
+        /// true if the operating system is 64-bit; otherwise, false.
+        /// </returns>
+        bool Is64BitOperatingSystem { get; }
+
+        /// <summary>
+        /// Determines whether the current process is a 64-bit process.
+        /// </summary>
+        /// <returns>
+        /// true if the process is 64-bit; otherwise, false.
+        /// </returns>
+        bool Is64BitProcess { get; }
+    }
+}
---
+++ SystemWrapper/EnvironmentWrap.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using SystemInterface;
+
+namespace SystemWrapper
+{
+    /// <summary>
+    /// Provides information about, and means to manipulate, the current environment and platform.
+    /// </summary>
+    /// <remarks>
+    /// This class provides default implementation using the <see cref="Environment"/> static class.
+    /// </remarks>
+    public class EnvironmentWrap : IEnvironment
+    {
+        public bool Is64BitOperatingSystem
+        {
+            get
+            {
+                return Environment.Is64BitOperatingSystem;
+            }
+        }
+
+        public bool Is64BitProcess
+        {
+            get
+            {
+                return Environment.Is64BitProcess;
+            }
+        }
+    }
+}
