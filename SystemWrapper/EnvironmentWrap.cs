7675b202041f71102994bcd644698f888cbcd170
Jozef Izso <jozef.izso@gmail.com>
2014-11-01 01:42:58 +0100

    Implemented EnvironmentWrap class.

--- SystemWrapper/EnvironmentWrap.cs
+++ SystemWrapper/EnvironmentWrap.cs
@@ -11,20 +11,120 @@ namespace SystemWrapper
     /// </remarks>
     public class EnvironmentWrap : IEnvironment
     {
-        public bool Is64BitOperatingSystem
+        public string CommandLine
+        {
+            get { return Environment.CommandLine; }
+        }
+
+        public string CurrentDirectory
         {
             get
             {
-                return Environment.Is64BitOperatingSystem;
+                return Environment.CurrentDirectory;
             }
+
+            set
+            {
+                Environment.CurrentDirectory = value;
+            }
+        }
+
+        public int CurrentManagedThreadId
+        {
+            get { return Environment.CurrentManagedThreadId; }
+        }
+
+        public int ExitCode
+        {
+            get
+            {
+                return Environment.ExitCode;
+            }
+
+            set
+            {
+                Environment.ExitCode = value;
+            }
+        }
+
+        public bool HasShutdownStarted
+        {
+            get { return Environment.HasShutdownStarted; }
+        }
+
+        public bool Is64BitOperatingSystem
+        {
+            get { return Environment.Is64BitOperatingSystem; }
         }
 
         public bool Is64BitProcess
         {
-            get
-            {
-                return Environment.Is64BitProcess;
-            }
+            get { return Environment.Is64BitProcess; }
+        }
+
+        public string MachineName
+        {
+            get { return Environment.MachineName; }
+        }
+
+        public string NewLine
+        {
+            get { return Environment.NewLine; }
+        }
+
+        public OperatingSystem OSVersion
+        {
+            get { return Environment.OSVersion; }
+        }
+
+        public int ProcessorCount
+        {
+            get { return Environment.ProcessorCount; }
+        }
+
+        public string StackTrace
+        {
+            get { return Environment.StackTrace; }
+        }
+
+        public string SystemDirectory
+        {
+            get { return Environment.SystemDirectory; }
+        }
+
+        public int SystemPageSize
+        {
+            get { return Environment.SystemPageSize; }
+        }
+
+        public int TickCount
+        {
+            get { return Environment.TickCount; }
+        }
+
+        public string UserDomainName
+        {
+            get { return Environment.UserDomainName; }
+        }
+
+        public bool UserInteractive
+        {
+            get { return Environment.UserInteractive; }
+        }
+
+        public string UserName
+        {
+            get { return Environment.UserName; }
+        }
+
+        public IVersion Version
+        {
+            get { return new VersionWrap(Environment.Version); }
+        }
+
+        public long WorkingSet
+        {
+            get { return Environment.WorkingSet; }
         }
     }
 }
