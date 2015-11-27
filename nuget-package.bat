892c07d6fc732cfb0b7d3c9bbd6ff4b3c1eca941
Jozef Izso <jozef.izso@gmail.com>
2015-11-27 21:45:22 +0100

    Added correct platform to the build script.

--- nuget-package.bat
+++ nuget-package.bat
@@ -1,6 +1,6 @@
-msbuild.exe SystemInterface\SystemInterface.csproj /p:Configuration="Release 4.0" /t:Build
-msbuild.exe SystemInterface\SystemInterface.csproj /p:Configuration="Release 4.5" /t:Build;Package;Publish
+msbuild.exe SystemInterface\SystemInterface.csproj /p:Configuration="Release 4.0" /p:Platform="AnyCPU" /t:Build
+msbuild.exe SystemInterface\SystemInterface.csproj /p:Configuration="Release 4.5" /p:Platform="AnyCPU" /t:Build;Package;Publish
 
-msbuild.exe SystemWrapper\SystemWrapper.csproj /p:Configuration="Release 4.0" /t:Build
-msbuild.exe SystemWrapper\SystemWrapper.csproj /p:Configuration="Release 4.5" /t:Build;Package;Publish
+msbuild.exe SystemWrapper\SystemWrapper.csproj /p:Configuration="Release 4.0" /p:Platform="AnyCPU" /t:Build
+msbuild.exe SystemWrapper\SystemWrapper.csproj /p:Configuration="Release 4.5" /p:Platform="AnyCPU" /t:Build;Package;Publish
 
