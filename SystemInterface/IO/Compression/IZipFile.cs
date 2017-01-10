1a6c927824029477521eccf8891a723afa6c5288
Chris Bush <christopher.bush@schneider-electric.com>
2017-01-10 04:23:14 -0500

    Added ZipFile wrappers

---
+++ SystemInterface/IO/Compression/IZipFile.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+using MicrosoftImpl = System.IO.Compression;
+
+namespace SystemInterface.IO.Compression {
+	public interface IZipFile : IStaticWrapper {
+		void CreateFromDirectory(string sourceDirectoryName, string destinationArchiveFileName);
+		void CreateFromDirectory(string sourceDirectoryName, string destinationArchiveFileName, MicrosoftImpl.CompressionLevel compressionLevel, bool includeBaseDirectory);
+		void CreateFromDirectory(string sourceDirectoryName, string destinationArchiveFileName, MicrosoftImpl.CompressionLevel compressionLevel, bool includeBaseDirectory, Encoding entryNameEncoding);
+		void ExtractToDirectory(string sourceArchiveFileName, string destinationDirectoryName);
+		void ExtractToDirectory(string sourceArchiveFileName, string destinationDirectoryName, Encoding entryNameEncoding);
+		IZipArchive Open(string archiveFileName, MicrosoftImpl.ZipArchiveMode mode);
+		IZipArchive Open(string archiveFileName, MicrosoftImpl.ZipArchiveMode mode, Encoding entryNameEncoding);
+		IZipArchive OpenRead(string archiveFileName);
+	}
+}
---
+++ SystemWrapper/IO/Compression/ZipArchiveWrap.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using SystemInterface.IO.Compression;
+using MicrosoftImpl = System.IO.Compression;
+
+namespace SystemWrapper.IO.Compression {
+	public class ZipArchiveWrap : IZipArchive {
+
+		public MicrosoftImpl.ZipArchive Instance { get; private set; }
+
+		public ZipArchiveWrap(MicrosoftImpl.ZipArchive Instance) {
+			this.Instance = Instance;
+		}
+
+		public ReadOnlyCollection<IZipArchiveEntry> Entries {
+			get {
+				return new ReadOnlyCollection<IZipArchiveEntry>(Instance.Entries.Select(s => new ZipArchiveEntryWrap(s)).Cast<IZipArchiveEntry>().ToList());
+			}
+		}
+
+		public MicrosoftImpl.ZipArchiveMode Mode {
+			get {
+				return Instance.Mode;
+			}
+		}
+
+		public IZipArchiveEntry CreateEntry(string entryName) {
+			return new ZipArchiveEntryWrap(Instance.CreateEntry(entryName));
+		}
+
+		public IZipArchiveEntry CreateEntry(string entryName, MicrosoftImpl.CompressionLevel compressionLevel) {
+			return new ZipArchiveEntryWrap(Instance.CreateEntry(entryName, compressionLevel));
+		}
+
+		public IZipArchiveEntry GetEntry(string entryName) {
+			return new ZipArchiveEntryWrap(Instance.GetEntry(entryName));
+		}
+
+		#region IDisposable Support
+		private bool disposedValue = false; // To detect redundant calls
+
+		protected virtual void Dispose(bool disposing) {
+			if (!disposedValue) {
+				if (disposing) {
+					Instance.Dispose();
+				}
+				disposedValue = true;
+			}
+		}
+		
+		public void Dispose() {
+			Dispose(true);
+		}
+		#endregion
+	}
+}
