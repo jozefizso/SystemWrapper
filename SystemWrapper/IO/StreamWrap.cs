b3da3857f69461107a2d52da052e857cde68b26d
Chris Bush <christopher.bush@schneider-electric.com>
2017-02-16 14:59:40 -0500

    Fix for #25. Added CopyTo methods for IStream

--- SystemWrapper/IO/StreamWrap.cs
+++ SystemWrapper/IO/StreamWrap.cs
@@ -385,6 +385,31 @@
             this.StreamInstance.WriteByte(value);
         }
 
+        public void CopyTo(IStream destination)
+        {
+            StreamInstance.CopyTo(destination.StreamInstance);
+        }
+
+        public void CopyTo(IStream destination, int bufferSize)
+        {
+            StreamInstance.CopyTo(destination.StreamInstance, bufferSize);
+        }
+
+        public async Task CopyToAsync(IStream destination)
+        {
+            await StreamInstance.CopyToAsync(destination.StreamInstance);
+        }
+
+        public async Task CopyToAsync(IStream destination, int bufferSize)
+        {
+            await StreamInstance.CopyToAsync(destination.StreamInstance, bufferSize);
+        }
+
+        public async Task CopyToAsync(IStream destination, int bufferSize, CancellationToken cancellationToken)
+        {
+            await StreamInstance.CopyToAsync(destination.StreamInstance, bufferSize, cancellationToken);
+        }
+
         #endregion
     }
 }
