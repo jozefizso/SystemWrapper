5b95743013a31bbddda84aeb924e38f9d716108c
Steve Elgas <s.elgas@techsmith.com>
2018-01-24 09:33:12 -0500

    Add source for nuget package to build script

--- CreatePackages.bat
+++ CreatePackages.bat
@@ -1,3 +1,3 @@
 @echo off
-.nuget\NuGet.exe install .nuget\packages.config -OutputDirectory packages
+.nuget\NuGet.exe install .nuget\packages.config -OutputDirectory packages -Source "https://www.nuget.org/api/v2"
 powershell.exe -NoProfile -ExecutionPolicy unrestricted -Command "& {Import-Module '.\packages\psake.*\tools\psake.psm1'; invoke-psake .\psake-project.ps1 -Task Pack %*; if ($LastExitCode -and $LastExitCode -ne 0) {write-host "ERROR CODE: $LastExitCode" -fore RED; exit $lastexitcode} }"
