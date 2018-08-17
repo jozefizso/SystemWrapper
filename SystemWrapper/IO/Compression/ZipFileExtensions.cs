a9e45e6c15586696339ef1f1cc69faa6b20b2eda
alexleen <alex_leendertsen@selinc.com>
2018-08-16 17:49:49 -0700

    Add ZipFileExtensions

---
+++ SystemWrapper/IO/Compression/ZipFileExtensions.cs
@@ -0,0 +1,18 @@
+using System.IO.Compression;
+using SystemInterface.IO.Compression;
+
+namespace SystemWrapper.IO.Compression
+{
+    public static class ZipFileExtensions
+    {
+        public static IZipArchiveEntry CreateEntryFromFile(this IZipArchive destination, string sourceFileName, string entryName)
+        {
+            return new ZipArchiveEntryWrap(destination.Instance.CreateEntryFromFile(sourceFileName, entryName));
+        }
+
+        public static IZipArchiveEntry CreateEntryFromFile(this IZipArchive destination, string sourceFileName, string entryName, CompressionLevel compressionLevel)
+        {
+            return new ZipArchiveEntryWrap(destination.Instance.CreateEntryFromFile(sourceFileName, entryName, compressionLevel));
+        }
+    }
+}
